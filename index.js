8115773fa7f546d607422deee79cd65f227e9316
DESKTOP-332GF4V\kkary <nambawon1@naver.com>
2023-03-01 23:20:02 +0900

    비밀 정보 보호

--- index.js
+++ index.js
@@ -1,13 +1,30 @@
 const express = require('express');
 const app = express()
-const port = 5000
+const port = 5000;
+const config = require('./config/key')
+
+const { User } = require("./models/User");
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://kkary:qwe123@boilerplate.uvsnwa7.mongodb.net/?retryWrites=true&w=majority')
+mongoose.connect(config.mongoURI)
 .then(()=>{console.log('MongoDB Connected..')})
 .catch(err=>{console.log(err)})
 
 
 app.get('/', (req,res) => {res.send('Hello world!')});
+
+app.post('/register',(req,res)=>{
+  //회원 가입 할 떄 필요한 정보들을 client에서 가져오면
+  //그것들을 데이터 베이스에 넣어준다.
+
+  const user = new User(req.body);
+  console.log(user);
+  user.save();
+});
+
+
 
 app.listen(port,()=>{console.log(`Example app listening on part ${port}`)});
